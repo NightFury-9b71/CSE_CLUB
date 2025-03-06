12d7d5b557ebefc328227a2d279eb3160b81f4c2
Nomanstine <nightfuryno9@gmail.com>
2025-03-07 00:08:39 +0600

    Header component rendered and working fine.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Header } from './components/Header';
+import Routes from './routes/RouteList';
 
 function App() {
   return (
-    <div className='h-screen flex flex-col items-center justify-center'>
-    
-      <h1 className='text-4xl font-bold'> Initial Setup </h1>
-      
-      <ul className='font-bold'>
-        <li>tailwindcss</li>
-        <li>lucide-react</li>
-        <li>react-router-dom</li>
-      </ul>
-
-    </div>
+    <Router>
+      <Routes />
+      <Header />
+    </Router>
   );
 }
 
